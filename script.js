83ba199a3e5c9e56741de7e0b17b2b4b7e40edc7
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-01-04 17:20:46 -0300

    Add Functio for the Button Fechar pedido and add functions for Bebias and Sobremesas

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-let Prato = "";
-let Bebida = "";
-let Sobremesa = "";
+class Pedido{
+    constructor(Prato, Bebida, Sobremesa, PratoPrice, BebidaPrice, SobremesaPrice, Total){
+        this.Prato = Prato;
+        this.Bebida = Bebida;
+        this.Sobremesa = Sobremesa;
+        
+        this.PratoPrice = PratoPrice;
+        this.BebidaPrice = BebidaPrice;
+        this.SobremesaPrice = SobremesaPrice;
+        this.Total = Total;
+    }
+}
 
+// ===--- Funções para os pratos
 function Prato1(div) {
     const Prato1 = document.getElementById("Prato1").style.borderColor = "#32B72F"
     const Prato2 = document.getElementById("Prato2").style.borderColor = "transparent"
     const Prato3 = document.getElementById("Prato3").style.borderColor = "transparent"
     const Prato4 = document.getElementById("Prato4").style.borderColor = "transparent"
     Prato = document.getElementById("Prato1").querySelector(".NameRequest").innerHTML;
+    PratoPrice = document.getElementById("Prato1").querySelector(".RequestPrice").innerHTML;
     console.log(Prato);
+    console.log(PratoPrice);
 }
 function Prato2(div) {
     const Prato1 = document.getElementById("Prato1").style.borderColor = "transparent"
@@ -16,7 +28,9 @@ function Prato2(div) {
     const Prato3 = document.getElementById("Prato3").style.borderColor = "transparent"
     const Prato4 = document.getElementById("Prato4").style.borderColor = "transparent"
     Prato = document.getElementById("Prato2").querySelector(".NameRequest").innerHTML;
+    PratoPrice = document.getElementById("Prato2").querySelector(".RequestPrice").innerHTML;
     console.log(Prato);
+    console.log(PratoPrice);
 }
 function Prato3(div) {
     const Prato1 = document.getElementById("Prato1").style.borderColor = "transparent"
@@ -24,7 +38,9 @@ function Prato3(div) {
     const Prato3 = document.getElementById("Prato3").style.borderColor = "#32B72F"
     const Prato4 = document.getElementById("Prato4").style.borderColor = "transparent"
     Prato = document.getElementById("Prato3").querySelector(".NameRequest").innerHTML;
+    PratoPrice = document.getElementById("Prato3").querySelector(".RequestPrice").innerHTML;
     console.log(Prato);
+    console.log(PratoPrice);
 }
 function Prato4(div) {
     const Prato1 = document.getElementById("Prato1").style.borderColor = "transparent"
@@ -32,12 +48,108 @@ function Prato4(div) {
     const Prato3 = document.getElementById("Prato3").style.borderColor = "transparent"
     const Prato4 = document.getElementById("Prato4").style.borderColor = "#32B72F"
     Prato = document.getElementById("Prato4").querySelector(".NameRequest").innerHTML;
+    PratoPrice = document.getElementById("Prato4").querySelector(".RequestPrice").innerHTML;
     console.log(Prato);
+    console.log(PratoPrice);
 }
 
-function ToggleMode(button){
-    const teste = document.getElementById("ResponseDisable");
+// ===--- Funções para as Bebidas
+function Bebida1(div) {
+    const Bebida1 = document.getElementById("Bebida1").style.borderColor = "#32B72F"
+    const Bebida2 = document.getElementById("Bebida2").style.borderColor = "transparent"
+    const Bebida3 = document.getElementById("Bebida3").style.borderColor = "transparent"
+    const Bebida4 = document.getElementById("Bebida4").style.borderColor = "transparent"
+    Bebida = document.getElementById("Bebida1").querySelector(".NameRequest").innerHTML;
+    BebidaPrice = document.getElementById("Bebida1").querySelector(".RequestPrice").innerHTML;
+    console.log(Bebida);
+    console.log(BebidaPrice);
+}
+function Bebida2(div) {
+    const Bebida1 = document.getElementById("Bebida1").style.borderColor = "transparent"
+    const Bebida2 = document.getElementById("Bebida2").style.borderColor = "#32B72F"
+    const Bebida3 = document.getElementById("Bebida3").style.borderColor = "transparent"
+    const Bebida4 = document.getElementById("Bebida4").style.borderColor = "transparent"
+    Bebida = document.getElementById("Bebida2").querySelector(".NameRequest").innerHTML;
+    BebidaPrice = document.getElementById("Bebida2").querySelector(".RequestPrice").innerHTML;
+    console.log(Bebida);
+    console.log(BebidaPrice);
+}
+function Bebida3(div) {
+    const Bebida1 = document.getElementById("Bebida1").style.borderColor = "transparent"
+    const Bebida2 = document.getElementById("Bebida2").style.borderColor = "transparent"
+    const Bebida3 = document.getElementById("Bebida3").style.borderColor = "#32B72F"
+    const Bebida4 = document.getElementById("Bebida4").style.borderColor = "transparent"
+    Bebida = document.getElementById("Bebida3").querySelector(".NameRequest").innerHTML;
+    BebidaPrice = document.getElementById("Bebida3").querySelector(".RequestPrice").innerHTML;
+    console.log(Bebida);
+    console.log(BebidaPrice);
+}
+function Bebida4(div) {
+    const Bebida1 = document.getElementById("Bebida1").style.borderColor = "transparent"
+    const Bebida2 = document.getElementById("Bebida2").style.borderColor = "transparent"
+    const Bebida3 = document.getElementById("Bebida3").style.borderColor = "transparent"
+    const Bebida4 = document.getElementById("Bebida4").style.borderColor = "#32B72F"
+    Bebida = document.getElementById("Bebida4").querySelector(".NameRequest").innerHTML;
+    BebidaPrice = document.getElementById("Bebida4").querySelector(".RequestPrice").innerHTML;
+    console.log(Bebida);
+    console.log(BebidaPrice);
+}
 
-    teste.style.backgroundColor = "#32B72F";
-    teste.innerText = "Fechar Pedido";
+// ===--- Funções para as Sobremesas
+function Sobremesa1(div) {
+    const Sobremesa1 = document.getElementById("Sobremesa1").style.borderColor = "#32B72F"
+    const Sobremesa2 = document.getElementById("Sobremesa2").style.borderColor = "transparent"
+    const Sobremesa3 = document.getElementById("Sobremesa3").style.borderColor = "transparent"
+    const Sobremesa4 = document.getElementById("Sobremesa4").style.borderColor = "transparent"
+    Sobremesa = document.getElementById("Sobremesa1").querySelector(".NameRequest").innerHTML;
+    SobremesaPrice = document.getElementById("Sobremesa1").querySelector(".RequestPrice").innerHTML;
+    console.log(Sobremesa);
+    console.log(SobremesaPrice);
+}
+function Sobremesa2(div) {
+    const Sobremesa1 = document.getElementById("Sobremesa1").style.borderColor = "transparent"
+    const Sobremesa2 = document.getElementById("Sobremesa2").style.borderColor = "#32B72F"
+    const Sobremesa3 = document.getElementById("Sobremesa3").style.borderColor = "transparent"
+    const Sobremesa4 = document.getElementById("Sobremesa4").style.borderColor = "transparent"
+    Sobremesa = document.getElementById("Sobremesa2").querySelector(".NameRequest").innerHTML;
+    SobremesaPrice = document.getElementById("Sobremesa2").querySelector(".RequestPrice").innerHTML;
+    console.log(Sobremesa);
+    console.log(SobremesaPrice);
+}
+function Sobremesa3(div) {
+    const Sobremesa1 = document.getElementById("Sobremesa1").style.borderColor = "transparent"
+    const Sobremesa2 = document.getElementById("Sobremesa2").style.borderColor = "transparent"
+    const Sobremesa3 = document.getElementById("Sobremesa3").style.borderColor = "#32B72F"
+    const Sobremesa4 = document.getElementById("Sobremesa4").style.borderColor = "transparent"
+    Sobremesa = document.getElementById("Sobremesa3").querySelector(".NameRequest").innerHTML;
+    SobremesaPrice = document.getElementById("Sobremesa3").querySelector(".RequestPrice").innerHTML;
+    console.log(Sobremesa);
+    console.log(SobremesaPrice);
+}
+function Sobremesa4(div) {
+    const Sobremesa1 = document.getElementById("Sobremesa1").style.borderColor = "transparent"
+    const Sobremesa2 = document.getElementById("Sobremesa2").style.borderColor = "transparent"
+    const Sobremesa3 = document.getElementById("Sobremesa3").style.borderColor = "transparent"
+    const Sobremesa4 = document.getElementById("Sobremesa4").style.borderColor = "#32B72F"
+    Sobremesa = document.getElementById("Sobremesa4").querySelector(".NameRequest").innerHTML;
+    SobremesaPrice = document.getElementById("Sobremesa4").querySelector(".RequestPrice").innerHTML;
+    console.log(Sobremesa);
+    console.log(SobremesaPrice);
+}
+
+// ===--- Função para ativar o botão verde
+function VerifyRequest(button){
+    const Verify = document.getElementById("ResponseDisable");
+    const Bar = document.getElementById("DownBar");
+    if(this.Prato !== undefined){
+        if(this.Bebida !== undefined){
+            if(this.Sobremesa !== undefined){
+                //Verify.style.backgroundColor = "#32B72F";
+                //Verify.style.display = "none"
+                Verify.parentNode.removeChild(Verify);
+                Bar.innerHTML = '<button id="Response">Fechar pedido</button>';
+                //Verify.innerText = "Fechar Pedido";
+            }
+        }   
+    }
 }
