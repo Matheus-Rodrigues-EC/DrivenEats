bed16d3a2cb0d3b1690c57fb9f1ad699f9755cd4
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-01-04 01:10:48 -0300

    Create and testing script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function ItemSelect(border) {
+    const select = document.querySelector("Request");
+
+    select.style.border = "#32B72F";
+}
+
+function ToggleMode(button){
+    const teste = document.getElementById("ResponseDisable");
+
+    teste.style.backgroundColor = "#32B72F";
+    teste.innerText = "Fechar Pedido";
+}
