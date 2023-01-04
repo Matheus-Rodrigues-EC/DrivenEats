6cecbdb9c3413202fc17c381f83a12919be34910
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-01-04 13:36:05 -0300

    Create and Configure Function for select plate and reconfigure request style

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-function ItemSelect(div) {
-    const select = document.querySelector();
+let Prato = "";
+let Bebida = "";
+let Sobremesa = "";
 
-    select.style.width = "172px";
-    select.style.height = "216px";
-    select.style.backgroundColor = "#FFFFFF";
-    select.style.borderRadius = "9px";
-    select.style.border = "5px solid #32B72F";
+function Prato1(div) {
+    const Prato1 = document.getElementById("Prato1").style.borderColor = "#32B72F"
+    const Prato2 = document.getElementById("Prato2").style.borderColor = "transparent"
+    const Prato3 = document.getElementById("Prato3").style.borderColor = "transparent"
+    const Prato4 = document.getElementById("Prato4").style.borderColor = "transparent"
+    Prato = document.getElementById("Prato1").querySelector(".NameRequest").innerHTML;
+    console.log(Prato);
+}
+function Prato2(div) {
+    const Prato1 = document.getElementById("Prato1").style.borderColor = "transparent"
+    const Prato2 = document.getElementById("Prato2").style.borderColor = "#32B72F"
+    const Prato3 = document.getElementById("Prato3").style.borderColor = "transparent"
+    const Prato4 = document.getElementById("Prato4").style.borderColor = "transparent"
+    Prato = document.getElementById("Prato2").querySelector(".NameRequest").innerHTML;
+    console.log(Prato);
+}
+function Prato3(div) {
+    const Prato1 = document.getElementById("Prato1").style.borderColor = "transparent"
+    const Prato2 = document.getElementById("Prato2").style.borderColor = "transparent"
+    const Prato3 = document.getElementById("Prato3").style.borderColor = "#32B72F"
+    const Prato4 = document.getElementById("Prato4").style.borderColor = "transparent"
+    Prato = document.getElementById("Prato3").querySelector(".NameRequest").innerHTML;
+    console.log(Prato);
+}
+function Prato4(div) {
+    const Prato1 = document.getElementById("Prato1").style.borderColor = "transparent"
+    const Prato2 = document.getElementById("Prato2").style.borderColor = "transparent"
+    const Prato3 = document.getElementById("Prato3").style.borderColor = "transparent"
+    const Prato4 = document.getElementById("Prato4").style.borderColor = "#32B72F"
+    Prato = document.getElementById("Prato4").querySelector(".NameRequest").innerHTML;
+    console.log(Prato);
 }
 
 function ToggleMode(button){
